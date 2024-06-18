2d104a768765323e867146073a506e81cff9b349
IvanMarek <marekivan08@gmail.com>
2024-06-18 04:11:04 -0300

    nuevo crud

---
+++ crud-examen/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { Usuario } from './usuarios/entities/usuario.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports:[TypeOrmModule.forRoot({
+    type: 'mysql',
+    host:'localhost',
+    port: 3306,
+    username: 'root',
+    database: 'CrudExamen',
+    entities: [Usuario],
+    synchronize: true,
+    }),UsuariosModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
